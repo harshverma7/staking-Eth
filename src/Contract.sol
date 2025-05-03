c373272ac77e0017da3b4a127557639f6d181f7a
Harsh <harshverma7k@gmail.com>
2025-05-04 00:13:51 +0530

    stake contract with bug in unstake function

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,25 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-contract Contract { }
+contract Stake {
+    uint256 public totalStaked;
+    mapping(address=> uint256) stakeBalance;
+
+    function stake(uint256 amount) public payable {
+        require(amount > 0, "Amount should be greater than 0");
+        require(msg.value == amount , "Amount should be equal to value sent");
+        stakeBalance[msg.sender] += amount;
+        totalStaked += amount;        
+    }
+
+    function unstake(uint256 amount) public payable {
+       require(stakeBalance[msg.sender] >= amount, "not enough balance");
+       stakeBalance[msg.sender] -= amount/2;
+       totalStaked -= amount/2;
+       payable(msg.sender).transfer(amount/2);        
+
+
+    }
+
+
+ }
